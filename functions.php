817d50a559a24c6bcb24901554f1420d2614b478
Raju Jeelaga <raju.jeelaga@hiddenbrains.in>
2020-12-22 11:15:35 +0530

    minor changes

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php
-function wordpress_theme_features(){
-	add_theme_support('title-tag');
+/**
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress Theme
+ */
+
+
+function wordpress_theme_scripts(){
+	wp_enqueue_style('wordpress_theme_styles', get_stylesheet_uri());
 }
 
-add_action('after_setup_theme', 'wordpress_theme_features');
-?>
+add_action('wp_enqueue_scripts', 'wordpress_theme_scripts');
+
--- header.php
+++ header.php
@@ -1,13 +1,40 @@
-<!DOCTYPE html>
-<html>
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress Theme
+ */
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-  	<?php wp_head();?>
-  	<meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>My WordPress Website</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
 </head>
 
-<body>
-	<header>
-		<h1>This is My Header</h1>
-	</header>
+<body <?php body_class(); ?>>
+	<div id="main-site" class="page-wrap">
+		<header>
+			<div class="site-header">
+				<div class="container">
+					<div class="smart-sticky-header">
+						<div class="logo">
+							<a href="#"><img src=""></a>
+						</div>
+						<div class="nav-links">
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+								<li><a href="#">Contact Us</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
